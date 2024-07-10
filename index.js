c5ed15506b96c00795d4613c420248ef13e3c9e0
harelask <harelmanicask@gmail.com>
2024-07-10 11:47:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 0909;
 
 app.use(cors());
 app.use(express.json());
