d67123c0d70850333eba991b268a613af6f37866
harelask <harelmanicask@gmail.com>
2024-07-12 19:22:46 +0530

    qqq

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 8081;
 const PORT = process.env.PORT || 8082;
 
 app.use(cors());
